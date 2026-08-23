d4bcaedd2ddc431f1237d96b4b532817a913103f
Analuiza <analuizahilariofaria@gmail.com>
2026-08-23 12:20:20 -0300

    Exer4

---
+++ Exer4/src/AAD/main.java
@@ -0,0 +1,15 @@
+package AAD;
+import javax.swing.*;
+public class main {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <50; i++) {
+			i += 1;
+			JOptionPane.showMessageDialog(null, i);
+		}
+			
+			
+
+	}
+
+}
